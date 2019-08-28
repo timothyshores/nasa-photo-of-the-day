069e83513641863bb1178bffc2c930e3cc7e588a
Timothy Shores <timothymshores@gmail.com>
2019-08-28 17:57:43 -0500

    Complete MVP

--- src/App.js
+++ src/App.js
@@ -5,22 +5,44 @@ import "./App.css";
 
 const App = () => {
 	const [pic, setPic] = useState("");
+	const [date, setDate] = useState(27);
 
 	useEffect(() => {
 		axios
-			.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+			.get(
+				"https://api.nasa.gov/planetary/apod?api_key=1pcDkqzVYtHxKLA4wapP50L4SSlkVyr5cyikedG3&date=2019-08-" +
+					date
+			)
 			.then(res => {
 				console.log(res.data);
 				setPic(res.data.url);
 			})
 			.catch(err => console.log(err));
-	}, [pic]);
+	}, [pic, date]);
 
 	return (
-		<div className="App">
+		<div
+			className="App"
+			style={{ color: "#fff", backgroundColor: "#000", height: "100vh" }}
+		>
+			<div>
+				<span
+					style={{ fontSize: "5rem" }}
+					onClick={() => setDate(date - 1)}
+					role="img"
+					aria-label="point-left"
+				>
+					{" "}
+					👈 
+				</span>
+				<img alt="NASA APOD" src={pic} />
+				<span style={{ fontSize: "5rem" }} role="img" aria-label="point-right">
+					{" "}
+					👉 
+				</span>
+			</div>
 			<h1>NASA</h1>
 			<h3>Astronomy Photo Of The Day</h3>
-			<img alt="NASA APOD" src={pic} />
 		</div>
 	);
 };
